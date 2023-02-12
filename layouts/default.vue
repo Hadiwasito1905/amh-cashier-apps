01e5b7c57e85fba0428dd64ff95568f47773b231
Noverry Ambo <noverryambo@gmail.com>
2023-02-12 22:47:52 +0700

    adding assets to pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="sideDrawer"
-      app
-    >
+    <v-navigation-drawer v-model="sideDrawer" app>
       <v-list>
         <v-list-item
           v-for="(item, i) in sideMenu"
@@ -27,57 +24,53 @@
       </v-container>
     </v-main>
 
-    <v-bottom-navigation
-      horizontal
-      height="10vh"
-      fixed
-      color="primary"
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="sideDrawer = !sideDrawer"
+    <v-bottom-navigation horizontal height="10vh" fixed color="primary" app>
+      <v-app-bar-nav-icon
+        @click.stop="sideDrawer = !sideDrawer"
         v-ripple="false"
         plain
       />
-        <v-btn v-for="(item, i) in bottomMenu"
-                :key="i"
-               :to="item.to"
-                v-ripple="false"
-                plain
-        >
-          <span>{{ item.title }}</span>
-          <v-icon>{{item.icon}}</v-icon>
-        </v-btn>
-        <v-spacer />
+      <v-btn
+        v-for="(item, i) in bottomMenu"
+        :key="i"
+        :to="item.to"
+        v-ripple="false"
+        plain
+      >
+        <span>{{ item.title }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+      <v-spacer />
     </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
+  name: "DefaultLayout",
+  data() {
     return {
       sideDrawer: false,
       sideMenu: [
         {
-          icon: 'mdi-account',
-          title: 'Account',
-          to: '/inspire'
+          icon: "mdi-account",
+          title: "Account",
+          to: "/inspire",
         },
         {
-          icon: 'mdi-bell',
-          title: 'Notification',
-          to: '/notification'
-        }
+          icon: "mdi-bell",
+          title: "Notification",
+          to: "/notification",
+        },
       ],
       bottomMenu: [
         {
-          icon: 'mdi-application',
-          title: 'App',
-          to: '/'
-        }
+          icon: "mdi-application",
+          title: "App",
+          to: "/",
+        },
       ],
-    }
-  }
-}
+    };
+  },
+};
 </script>
